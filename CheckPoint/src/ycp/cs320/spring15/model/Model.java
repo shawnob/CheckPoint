3a911deec6c379dcf077d510bbdb990c26855319
Ben Langsdale <blangsda@ycp.edu>
2015-03-12 15:23:52 -0400

    Model class partially implemented

--- CheckPoint/src/ycp/cs320/spring15/model/Model.java
+++ CheckPoint/src/ycp/cs320/spring15/model/Model.java
@@ -1,5 +1,41 @@
 package ycp.cs320.spring15.model;
 
-public class Model {
+import java.util.List;
 
+public class Model {
+	
+	UserList users = new UserList();
+	
+	public User getUser(String username)
+	{
+		return users.getUser(username);
+	}
+	
+	public boolean addUser(String uname, String pword)
+	{
+		return users.CreateUser(uname, pword);
+	}
+	
+	public void deleteUser(User deluser)
+	{
+		users.deleteUser(deluser);
+	}
+	
+	public void deleteUser(String username)
+	{
+		User deluser = users.getUser(username);
+		users.deleteUser(deluser);
+	}
+	
+	public UserList getUserList()
+	{
+		return users;
+	}
+	
+	public List<User> getListofUsers()
+	{
+		return users.getUserList();
+	}
+	
+	
 }
