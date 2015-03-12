508199520574bb4cb69b7af89da4d3994ebc7e65
Zackeree <zhenry@ycp.edu>
2015-03-12 15:49:40 -0400

    Added a few methods to Assignment and AssignmentList

--- CheckPoint/src/ycp/cs320/spring15/model/Assignment.java
+++ CheckPoint/src/ycp/cs320/spring15/model/Assignment.java
@@ -1,5 +1,22 @@
 package ycp.cs320.spring15.model;
 
 public class Assignment {
+	
+	//0 for Multiple Choice, 1 for Fill in the Blank
+	
+	private boolean questionType;
+	
+	/*
+	 * Getter and setter methods
+	 */
+	public boolean getQuestionType() {
+		return questionType;
+	}
+	
+	public void setQuestionType(boolean type){
+		questionType = type;
+	}
+	
+	//todo:implement
 
 }
--- CheckPoint/src/ycp/cs320/spring15/model/AssignmentList.java
+++ CheckPoint/src/ycp/cs320/spring15/model/AssignmentList.java
@@ -2,4 +2,14 @@ package ycp.cs320.spring15.model;
 
 public class AssignmentList {
 
+	private Assignment[] assignment;
+	
+	/*
+	 * Get an assignment from the array
+	 */
+	public Assignment getAssignment(int assignmentNumber) {
+		return assignment[assignmentNumber];
+	}
+	
+	//todo:implement
 }
