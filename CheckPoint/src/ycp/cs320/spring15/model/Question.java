ba67efed190f344fa7536486dd31f948602c5dca
Zackeree <zhenry@ycp.edu>
2015-03-18 13:32:21 -0400

    Finally got it working. :>

--- CheckPoint/src/ycp/cs320/spring15/model/Question.java
+++ CheckPoint/src/ycp/cs320/spring15/model/Question.java
@@ -2,44 +2,30 @@ package ycp.cs320.spring15.model;
 
 public class Question {
 	
-	/*
 	// Multiple Choice and Fill in the Blank respectively.
 	public enum questionType{
 		MC(0), FIB(1);
 		
-		public int TYPE;
+		private int type;
 		
-		private Question(int type) {
-			TYPE = type;
+		private questionType(int type) {
+			this.type = type;
 		}
 		
-		private int getQuestionType() {
-			return TYPE;
+		public void setQuestionType(int type) {
+			this.type = type;
 		}
-	}*/
+		
+		public int getQuestionType() {
+			return this.type;
+		}
+	}
 	
-	//false for Multiple Choice, true for Fill in the Blank
-	private boolean questionType;
 	//correct answers for multiple choice and fill in the blank
 	private int correctAnswerMC;
 	private String correctAnswerFIB;
-	/*
-	public Question(enum type, int correctAns) {
 		
-	}
-	
-	public Question(enum type, String correctAns) {
-		
-	}*/
-	
 	//getter/setter
-	public boolean questionType() {
-		return questionType; 
-	}
-	
-	public void setQuestionType(boolean type) {
-		questionType = type;
-	}
 	
 	public int getCorrectAnsMC() {
 		return correctAnswerMC;
