4ee5f54e3f628d8631b98109c14c645d330d32bd
Zackeree <zhenry@ycp.edu>
2015-03-18 13:18:26 -0400

    Trying to set the questionType field as an enum.

--- CheckPoint/src/ycp/cs320/spring15/model/Question.java
+++ CheckPoint/src/ycp/cs320/spring15/model/Question.java
@@ -2,11 +2,35 @@ package ycp.cs320.spring15.model;
 
 public class Question {
 	
+	/*
+	// Multiple Choice and Fill in the Blank respectively.
+	public enum questionType{
+		MC(0), FIB(1);
+		
+		public int TYPE;
+		
+		private Question(int type) {
+			TYPE = type;
+		}
+		
+		private int getQuestionType() {
+			return TYPE;
+		}
+	}*/
+	
 	//false for Multiple Choice, true for Fill in the Blank
 	private boolean questionType;
 	//correct answers for multiple choice and fill in the blank
 	private int correctAnswerMC;
 	private String correctAnswerFIB;
+	/*
+	public Question(enum type, int correctAns) {
+		
+	}
+	
+	public Question(enum type, String correctAns) {
+		
+	}*/
 	
 	//getter/setter
 	public boolean questionType() {
