7bf62411e469d15d43be62b37a68f5db20595327
msavoca <msavoca@ycp.edu>
2015-04-29 13:30:57 -0400

    fix

--- CheckPointWebapp/src/ycp/cs320/spring15/webapp/servlets/Controller.java
+++ CheckPointWebapp/src/ycp/cs320/spring15/webapp/servlets/Controller.java
@@ -1,7 +1,7 @@
 package ycp.cs320.spring15.webapp.servlets;
 
 import ycp.cs320.spring15.model.Course;
-import ycp.cs320.spring15.model.QuestionList;
+import ycp.cs320.spring15.model.Quiz;
 import ycp.cs320.spring15.model.User;
 import ycp.cs320.spring15.persist.DatabaseProvider;
 import ycp.cs320.spring15.persist.IDatabase;
