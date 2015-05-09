001d874c343ae30d1b1d57d0af28df8ccab99d9e
Zackeree <zhenry@ycp.edu>
2015-05-09 19:26:57 -0400

    Created test classes.

---
+++ CheckPointTests/src/ycp/cs320/spring15/webapp/ControllerTest.java
@@ -0,0 +1,10 @@
+package ycp.cs320.spring15.webapp;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ControllerTest {
+	Controller c = new Controller();
+
+}
