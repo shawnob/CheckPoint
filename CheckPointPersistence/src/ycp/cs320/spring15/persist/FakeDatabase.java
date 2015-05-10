789a83f9af4f3eada8405e611b58dece6e8075c3
msavoca <msavoca@ycp.edu>
2015-05-10 10:17:57 -0400

    taker update

--- CheckPointPersistence/src/ycp/cs320/spring15/persist/FakeDatabase.java
+++ CheckPointPersistence/src/ycp/cs320/spring15/persist/FakeDatabase.java
@@ -51,8 +51,15 @@ public class FakeDatabase implements IDatabase {
 		
 		
 		quizList.add(new Quiz("The Quiz", userList.getUser("marvin") , new Course("cs320") , 666));
-
-		this.getQuiz(666).addQuestion(new Question(1,0, "Whats your name", choices, 0));
+		
+		//short answer questions
+		this.getQuiz(666).addQuestion(new Question(0,0, "Whats your name", null, 5));
+		this.getQuiz(666).addQuestion(new Question(0,0, "Whats", null, 6));
+		
+		//multiple cloice questions
+//		this.getQuiz(666).addQuestion(new Question(1,0, "Whats your name", choices, 0));
+//		this.getQuiz(666).addQuestion(new Question(1,1, "n2", choices, 1));
+//		this.getQuiz(666).addQuestion(new Question(1,2, "n3", choices, 2));
 
 		courseList.add(new Course("Truffles 101"));
 		courseList.add(new Course("The-Spells-of-Starswirl-the-Bearded"));
--- CheckPointWebapp/src/ycp/cs320/spring15/webapp/servlets/QuizTakerServlet.java
+++ CheckPointWebapp/src/ycp/cs320/spring15/webapp/servlets/QuizTakerServlet.java
@@ -13,6 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 
 
 
+
+
+
+import ycp.cs320.spring15.model.Quiz;
 import ycp.cs320.spring15.model.User;
 import ycp.cs320.spring15.persist.DatabaseProvider;
 import ycp.cs320.spring15.persist.FakeDatabase;
@@ -23,40 +27,45 @@ import ycp.cs320.spring15.persist.IDatabase;
 
 public class QuizTakerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	private int questionnum = 0;
 
-	
+	private int questionnum = 0;
+	private int rcount = 0;
+
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
 		System.out.println("doGetT called");
-		System.out.println(questionnum);
+		//System.out.println(questionnum);
 		Controller controller = new Controller();
 
 		int quizID = 666;
 
 		//int questionnum = controller.retquestnum(quizID);
 		String question = controller.retquest(quizID, questionnum);
-		int type = controller.retquesttype(quizID, questionnum);
+		int type = controller.getQuiz(quizID).getQuestion(questionnum).getQuestionType();
 		String[] choices = controller.retquestchoices(quizID, questionnum);
-		
+		Quiz theQuiz = controller.getQuiz(quizID);
 
-
-		req.setAttribute("choice1", choices[0]);
-		req.setAttribute("choice2", choices[1]);
-		req.setAttribute("choice3", choices[2]);
+		req.setAttribute("rcount", rcount);
+		System.out.println(theQuiz.getQuestion(questionnum).getQuestionType());
+		if (theQuiz.getQuestion(questionnum).getQuestionType() == 1){
+			req.setAttribute("choice1", choices[0]);
+			req.setAttribute("choice2", choices[1]);
+			req.setAttribute("choice3", choices[2]);
+		}
 		req.setAttribute("questionnum", questionnum);
 		req.setAttribute("question", question);
 		req.setAttribute("type", type);
-		
-		
+
+
 		//req.setAttribute("answer", answer);
-		
+
 		questionnum++;
 
 		//Calls the login.jsp file containing the html and css
+		//doPost(req, resp);
 		req.getRequestDispatcher("/_view/quiztaker.jsp").forward(req, resp);
 	}
 
@@ -66,30 +75,52 @@ public class QuizTakerServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		Controller controller = new Controller();
-
+		Quiz theQuiz = controller.getQuiz(666);
 		System.out.println("doPostT called");
-		String answer = req.getParameter("answer");
-		String select1 = req.getParameter("select1");
-		String select2 = req.getParameter("select2");
-		String select3 = req.getParameter("select3");
-		
-		System.out.println("answer=" + answer);
-		System.out.println("1=" + select1);
-		System.out.println("2=" + select2);
-		System.out.println("3=" + select3);
-		
-		
-		
-		if(controller.getQuiz(0).getQuestion(0).CheckAnswer(answer, 0)){
-			req.setAttribute("result", "Correct!");
-		}else{
-			req.setAttribute("result", "Incorrect!");
+
+		String answer = req.getParameter("submited");
+
+		System.out.println("A=" + answer);
+
+		if (theQuiz.getQuestion(questionnum-1).getQuestionType() == 1){
+			String[] choices = controller.retquestchoices(666, questionnum-1);
+			int checked = 0;	
+			if (answer.equals(choices[0])){
+				checked = 0;
+			}
+			if (answer.equals(choices[1])){
+				checked = 1;
+			}
+			if (answer.equals(choices[2])){
+				checked = 2;
+			}
+
+			if (controller.getQuiz(666).getQuestion(questionnum-1).getCorrectAns() == checked){
+				//req.setAttribute("result", "Correct!");
+				rcount++;
+			}else{
+				//req.setAttribute("result", "Incorrect!");
+			}
 		}
 
-		//req.setAttribute("errorMessage", errorMessage);
-		//req.setAttribute("result", result);
-		req.getRequestDispatcher("/_view/quiztaker.jsp").forward(req, resp);
-		// Decode form parameters and dispatch to controller
+		if (theQuiz.getQuestion(questionnum-1).getQuestionType() == 0){
+			
+			int answer2 = Integer.parseInt(answer);
+			
+			if (controller.getQuiz(666).getQuestion(questionnum-1).getCorrectAns() == answer2){
+				//req.setAttribute("result", "Correct!");
+				rcount++;
+			}else{
+				//req.setAttribute("result", "Incorrect!");
+			}
+		}
+		
+		if (theQuiz.getNumQuestions() == questionnum) {
+			questionnum = 0;
+			req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
+		}else{
+			doGet(req, resp);
+		}
 
 	}
 }
